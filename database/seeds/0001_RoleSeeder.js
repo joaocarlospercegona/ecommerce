6afe00c05a5da9e615d30e3702189def08537b5e
joaocarlospercegona <joaokarlospercegona@gmail.com>
2021-03-18 14:20:43 -0300

    role and seeders

---
+++ database/seeds/0001_RoleSeeder.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RoleSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Role = use('Role')
+
+class RoleSeeder {
+  async run () {
+    //cria o admin
+    await Role.create({
+      name: 'Admin',
+      slug: 'admin',
+      description: 'Administrador do sistema!'
+    })
+    //cria o cargo de gerente
+    await Role.create({
+      name: 'Menager',
+      slug: 'menager',
+      description: 'Gerente da loja'
+    })
+    //cria o cardo do cliente
+    await Role.create({
+      name: 'Client',
+      slug: 'client',
+      description: 'Cliente da loja'
+    })
+  }
+}
+
+module.exports = RoleSeeder
